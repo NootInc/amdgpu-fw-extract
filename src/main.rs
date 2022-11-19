baac51133998a32977bfbf361e96944dfffd7979
Visual <30368284+ChefKissInc@users.noreply.github.com>
2022-11-19 22:58:57 +0200

    Fix JT logic
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
     let input = std::env::args().nth(2).unwrap();
     let output = std::env::args().nth(3).unwrap();
 
-    let mut c = std::io::Cursor::new(std::fs::read(input).unwrap());
+    let mut c = std::io::Cursor::new(std::fs::read(&input).unwrap());
     c.set_position(20);
     let ucode_size = c.read_u32::<LittleEndian>().unwrap() as usize;
     let ucode_off = c.read_u32::<LittleEndian>().unwrap() as u64;
@@ -107,15 +107,26 @@ fn main() {
             c.seek(SeekFrom::Current(4)).unwrap();
             let jt_off = c.read_u32::<LittleEndian>().unwrap() as u64;
             let jt_size = c.read_u32::<LittleEndian>().unwrap() as usize * 4;
+            let mec = input.ends_with("_mec.bin") || input.ends_with("_mec2.bin");
             c.set_position(ucode_off);
-            let mut buf = vec![0; ucode_size];
+            let mut buf = vec![
+                0;
+                if mec {
+                    ucode_size - jt_size
+                } else {
+                    ucode_size
+                }
+            ];
             c.read_exact(buf.as_mut_slice()).unwrap();
             std::fs::write(&output, buf).unwrap();
             if jt_off != 0 && jt_size != 0 {
-                c.set_position(jt_off);
-                let mut buf = vec![0; jt_size];
-                c.read_exact(buf.as_mut_slice()).unwrap();
-                std::fs::write(output + ".jt", buf).unwrap();
+                println!("jt_off: {:#X}", jt_off);
+                if mec {
+                    c.set_position(ucode_off + (ucode_size - jt_size) as u64);
+                    let mut buf = vec![0; jt_size];
+                    c.read_exact(buf.as_mut_slice()).unwrap();
+                    std::fs::write(output + ".jt", buf).unwrap();
+                }
             }
         }
         v => eprintln!("Unknown firmware type '{}'", v),
